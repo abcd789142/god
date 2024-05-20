5f02289cad30da13fa691450d1a7456d5c83ae27
abcd789142 <abcd145365@outlook.com>
2024-05-20 12:31:33 +0800

    Update kill 1111.lua

--- kill 1111.lua
+++ kill 1111.lua
@@ -1,17 +1,17 @@
-local OrionLib = loadstring(gameHttpGet(('httpsraw.githubusercontent.comleenZzZzZopen_source_leenmainopen%20sourcezada')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/leenZzZzZ/open_source_leen/main/open%20sourcezada')))()
 local Player = game.Players.LocalPlayer
-local Window = OrionLibMakeWindow({Name = 鸭脖验证系统, HidePremium = false, SaveConfig = true, IntroEnable = false})
+local Window = OrionLib:MakeWindow({Name = "鸭脖验证系统", HidePremium = false, SaveConfig = true, IntroEnable = false})
 
-OrionLibMakeNotification({
-	Name = 欢迎使用,
-	Content = 你好..Player.Name.. ,
-	Image = rbxassetid14229447778,
+OrionLib:MakeNotification({
+	Name = "欢迎使用",
+	Content = "你好"..Player.Name.." ",
+	Image = "rbxassetid://14229447778",
 	Time = 5
 })
 
-_G.Key = BZARNGQYDFYP
-_G.KeyPERM = KLdwkqodKQODMASODQl	
-_G.KeyInput = string
+_G.Key = "BZARNGQYDFYP"
+_G.KeyPERM = "KLdwkqodKQODMASODQl"	
+_G.KeyInput = "string"
 
 local debounce = false;
 
@@ -19,115 +19,115 @@ function MakeScriptHub()
 	if debounce then return; end;
 	debounce = true;
 
-	OrionLibMakeNotification({
-		Name = 密钥不对！,
-		Content = 你写错了哥们,
-		Image = rbxassetid6353957304,
+	OrionLib:MakeNotification({
+		Name = "密钥不对！",
+		Content = "你写错了哥们",
+		Image = "rbxassetid://6353957304",
 		Time = 5
 	})
 
-	local Window = OrionLibMakeWindow({Name = 鸭脖脚本1.0, SearchBar = { Default = '查找游戏', ClearTextOnFocus = true }, HidePremium = false, SaveConfig = true, ConfigFolder = OrionTest})
+	local Window = OrionLib:MakeWindow({Name = "鸭脖脚本1.0", SearchBar = { Default = '查找游戏', ClearTextOnFocus = true }, HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
-                local Tab = WindowMakeTab({
-    Name = 常用,
-    Icon = rbxassetid7734068321,
+                local Tab = Window:MakeTab({
+    Name = "常用",
+    Icon = "rbxassetid://7734068321",
     PremiumOnly = false
 })
 
-TabAddTextbox({
-	Name = 移动速度(部分游戏不可用),
-	Default = ,
+Tab:AddTextbox({
+	Name = "移动速度(部分游戏不可用)",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
 	end	 
 })
  
-TabAddTextbox({
-	Name = 跳跃高度(部分游戏不可用),
-	Default = ,
+Tab:AddTextbox({
+	Name = "跳跃高度(部分游戏不可用)",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
 	end	 
 })
 
-TabAddTextbox({
-	Name = 重力设置(部分游戏不可用),
-	Default = ,
+Tab:AddTextbox({
+	Name = "重力设置(部分游戏不可用)",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
 		game.Workspace.Gravity = Value
 	end
 })
-TabAddButton({
-	Name = 爬墙,
+Tab:AddButton({
+	Name = "爬墙",
 	Callback = function()
-loadstring(gameHttpGet(httpspastebin.comrawzXk4Rq2r))()
+loadstring(game:HttpGet("https://pastebin.com/raw/zXk4Rq2r"))()
 end
 })
 
-TabAddButton({
-	Name = 防止掉线（反挂机）,
+Tab:AddButton({
+	Name = "防止掉线（反挂机）",
 	Callback = function()
-	print(Anti Afk On)
-		local vu = gameGetService(VirtualUser)
-		gameGetService(Players).LocalPlayer.Idledconnect(function()
-		   vuButton2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	print("Anti Afk On")
+		local vu = game:GetService("VirtualUser")
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
+		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 		   wait(1)
-		   vuButton2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 		end)
   	end
 })
 
-TabAddButton({
-	Name = 甩飞别人,
+Tab:AddButton({
+	Name = "甩飞别人",
 	Callback = function()
-     loadstring(gameHttpGet(httpspastebin.comrawGnvPVBEi))()
+     loadstring(game:HttpGet("https://pastebin.com/raw/GnvPVBEi"))()
   	                 end    
                 })
 
-	local Tab = WindowMakeTab({
-		Name = 俄亥俄州 秒变大蛇脚本,
-		Icon = rbxassetid11348590078,
+	local Tab = Window:MakeTab({
+		Name = "俄亥俄州 秒变大蛇脚本",
+		Icon = "rbxassetid://11348590078",
 		PremiumOnly = false
 	})
 
-	TabAddButton({
-		Name = 子追追踪,
+	Tab:AddButton({
+		Name = "子追追踪",
 		Callback = function()
-			setclipboard(key individual ;()
-			toclipboard(key individual ;()
-			repeat wait() until gameIsLoaded()
-			loadstring(gameHttpGet(httpsraw.githubusercontent.comabcd789142godmain(ft.%20Helper%20Prod.%20MyWorld).lua))()
+			setclipboard("key individual ;(")
+			toclipboard("key individual ;(")
+			repeat wait() until game:IsLoaded()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/abcd789142/god/main/(ft.%20Helper%20Prod.%20MyWorld).lua"))()
 		end    
 	})
 
-	TabAddButton({
-		Name = 自瞄Q,
+	Tab:AddButton({
+		Name = "自瞄Q",
 		Callback = function()
-			setclipboard(key individual ;()
-			toclipboard(key individual ;()
-			repeat wait() until gameIsLoaded()
-			loadstring(gameHttpGet(httpsraw.githubusercontent.comabcd789142godmainaimbotq.lua))()
+			setclipboard("key individual ;(")
+			toclipboard("key individual ;(")
+			repeat wait() until game:IsLoaded()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/abcd789142/god/main/aimbotq.lua"))()
 		end    
 	})
 
-	TabAddButton({
-		Name = 自瞄V,
+	Tab:AddButton({
+		Name = "自瞄V",
 		Callback = function()
-			setclipboard(key individual ;()
-			toclipboard(key individual ;()
-			repeat wait() until gameIsLoaded()
-			loadstring(gameHttpGet(httpsraw.githubusercontent.comabcd789142godmainaimbotv.lua))()
+			setclipboard("key individual ;(")
+			toclipboard("key individual ;(")
+			repeat wait() until game:IsLoaded()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/abcd789142/god/main/aimbotv.lua"))()
 		end    
 	})
 end
 function IncorrectKeyNotification()
-	OrionLibMakeNotification({
-		Name = 密钥不对！,
-		Content = 是不是写错了啊哥们,
-		Image = rbxassetid6353958325,
+	OrionLib:MakeNotification({
+		Name = "密钥不对！",
+		Content = "是不是写错了啊哥们",
+		Image = "rbxassetid://6353958325",
 		Time = 5
 	})
 end
@@ -142,15 +142,15 @@ function verify()
 	end
 end
 
-local Tab = WindowMakeTab({
-	Name = 密钥,
-	Icon = rbxassetid221748793,
+local Tab = Window:MakeTab({
+	Name = "密钥",
+	Icon = "rbxassetid://221748793",
 	PremiumOnly = false
 })
 
-TabAddTextbox({
-	Name = 输入密钥,
-	Default = ,
+Tab:AddTextbox({
+	Name = "输入密钥",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
 		_G.KeyInput = Value
